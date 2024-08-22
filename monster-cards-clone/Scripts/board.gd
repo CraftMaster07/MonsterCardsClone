5b34307f86a52f19bc729acce1a45f51ce1bd42c
CraftMaster07 <99757789+CraftMaster07@users.noreply.github.com>
2024-08-23 00:15:20 +0300

    added card de\select SFX

--- monster-cards-clone/Scripts/board.gd
+++ monster-cards-clone/Scripts/board.gd
@@ -3,6 +3,9 @@ extends Node2D
 var selected_card : Card
 @onready var hand := $Hand
 
+@onready var select_stream : AudioStream = load("res://assets/audio/sounds/select.mp3")
+@onready var deselect_stream : AudioStream = load("res://assets/audio/sounds/deselect.mp3")
+
 func _ready() -> void:
 	for card in hand.get_children():
 		card.connect("card_selected", select_card)
@@ -13,5 +16,18 @@ func _process(delta: float) -> void:
 func select_card(card):
 	if selected_card == card:
 		selected_card = null
+		play_sound_at_card(card, deselect_stream)
 	else:
 		selected_card = card
+		play_sound_at_card(card, select_stream)
+		
+
+func play_sound_at_card(card: Card, sound: AudioStream) -> void:
+	var sound_player = AudioStreamPlayer2D.new()
+	sound_player.bus = "SFX"
+	card.add_child(sound_player)
+	sound_player.position = card.position
+	sound_player.stream = sound
+	sound_player.play()
+	sound_player.connect("finished", sound_player.queue_free)
+		
--- monster-cards-clone/scripts/card.gd
+++ monster-cards-clone/scripts/card.gd
@@ -1,8 +1,34 @@
 extends TextureButton
 class_name Card
 
+@export var display_name : String
+@export var description : String
+@export var image : Texture2D
+
+@export var cost : int = 1:
+	set(value):
+		cost = clamp(value, 0, 999)
+@export var damage : int = 1:
+	set(value):
+		damage = clamp(value, 0, 999)
+@export var health : int = 1:
+	set(value):
+		health = clamp(value, 0, 999)
+
+var current_cost : int
+var current_damage : int
+var current_health : int
+
+
 signal card_selected(card)
 
 func _on_button_up() -> void:
 	print("hi")
 	card_selected.emit(self)
+
+
+func take_damage(amount : int) -> void:
+	current_health -= amount
+	if current_health <= 0:
+		queue_free()
+	
