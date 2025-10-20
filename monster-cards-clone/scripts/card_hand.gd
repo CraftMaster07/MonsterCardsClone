cf32b1c39d79ddc668f389bc766f608d66385982
rainmas <shmeritamar@gmail.com>
2025-10-20 22:37:39 +0300

    cards can actually go in the slots now omg

--- monster-cards-clone/scripts/card_hand.gd
+++ monster-cards-clone/scripts/card_hand.gd
@@ -5,6 +5,7 @@ extends Control
 @export var animation_trans : Tween.TransitionType
 @onready var card_front := $CardFront
 @onready var base_position : Vector2 = card_front.position
+@onready var area2d := $CardFront/Area2D
 
 var touched : bool = false
 enum DragState {RESTING, DRAGGING, FINISHING_DRAGGING}
@@ -12,6 +13,7 @@ var drag_state : DragState = DragState.RESTING
 @export var dragback_time : float = 0.5
 @export var dragback_trans : Tween.TransitionType = Tween.TRANS_ELASTIC
 @export var dragback_ease : Tween.EaseType = Tween.EASE_OUT
+var overlapping_slot_areas : Array[Area2D]
 var tween: Tween
 
 func _process(_delta: float) -> void:
@@ -22,12 +24,26 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_released("click") and drag_state == DragState.DRAGGING:
 		drag_state = DragState.FINISHING_DRAGGING
 		
+		if not overlapping_slot_areas.is_empty():
+			goto_nearest_overlapping_area()
+			return
+		
 		animate_to_position(base_position, dragback_trans, dragback_time, dragback_ease)
 		await tween.finished
 		drag_state = DragState.RESTING
 		
 		if card_front.is_hovered():
 			_on_mouse_entered()
+
+func goto_nearest_overlapping_area():
+	overlapping_slot_areas.sort_custom(area_distance_compare)
+	var target_position := overlapping_slot_areas[0].global_position - size/2
+	animate_to_position(target_position, animation_trans, animation_length, Tween.EASE_IN_OUT, true)
+
+func area_distance_compare(area1 : Area2D, area2 : Area2D):
+	if area1.global_position.distance_to(area2d.global_position) < area2.global_position.distance_to(area2d.global_position):
+		return true
+	return false
 
 func update_base_position():
 	"""Updates the base_position variable to the current card_front position"""
@@ -51,14 +67,22 @@ func _on_mouse_exited() -> void:
 	
 	animate_to_position(base_position, animation_trans, animation_length)
 
-
-func animate_to_position(pos, trans_type, length, ease_type = Tween.EASE_IN_OUT):
+func animate_to_position(pos, trans_type, length, ease_type = Tween.EASE_IN_OUT, global = false):
 	if tween:
 		tween.kill()
 	
 	tween = create_tween()
-	tween.tween_property(card_front, "position", pos, length).set_trans(trans_type).set_ease(ease_type)
-
+	if global:
+		tween.tween_property(card_front, "global_position", pos, length).set_trans(trans_type).set_ease(ease_type)
+	else:
+		tween.tween_property(card_front, "position", pos, length).set_trans(trans_type).set_ease(ease_type)
 
 func _on_card_front_button_down() -> void:
 	drag_state = DragState.DRAGGING
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	overlapping_slot_areas.append(area)
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	if area in overlapping_slot_areas:
+		overlapping_slot_areas.erase(area)
--- monster-cards-clone/scripts/card_slot.gd
+++ monster-cards-clone/scripts/card_slot.gd
@@ -1,7 +1,11 @@
 extends Control
 
 
-
-
 func _on_mouse_entered() -> void:
 	print_rich("The mouse just [color=red][shake]entered[/shake][/color] me uwu")
+
+
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("an area has entered me ", area)
