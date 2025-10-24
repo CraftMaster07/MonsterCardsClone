c696a18ec16ca1b378c94b85c8a8a3f684e6df1d
CraftMaster07 <mogobking@gmail.com>
2025-10-24 17:31:12 +0300

    started adding host and join buttons
    not really functioning

--- monster-cards-clone/scripts/multiplayer.gd
+++ monster-cards-clone/scripts/multiplayer.gd
@@ -1,6 +1,28 @@
 extends Node
 
+@onready var host_button = get_parent().get_node("HostButton")
+@onready var join_button = get_parent().get_node("JoinButton")
+
 
 var peer = ENetMultiplayerPeer.new()
-var ip_address = "127.0.0.1"
-var port = 1919
+var PORT = 59009
+
+@export var player_field_scene: PackedScene
+
+
+func _on_host_button_pressed() -> void:
+	disable_buttons()
+
+	peer.create_server(PORT)
+	multiplayer.multiplayer_peer = peer
+
+	var your_player_scene = player_field_scene.instantiate()
+	add_child(your_player_scene)
+
+
+func disable_buttons():
+	host_button.disabled = true
+	host_button.visible = false
+	join_button.disabled = true
+	join_button.visible = false
+	
