3ea3100fa539a01b00a8fc26fc10da43ffde5290
Rainmas <shmeritamar@gmail.com>
2025-10-31 16:13:06 +0200

    fixed some spacing between functions

--- monster-cards-clone/Scripts/Main Menu/main_menu.gd
+++ monster-cards-clone/Scripts/Main Menu/main_menu.gd
@@ -4,13 +4,14 @@ extends Control
 @export var HostMenu: Control
 @export var JoinMenu: Control
 
+
 func _on_idk_button_pressed() -> void:
 	print("why would you press this?")
 
 
 func _on_deck_button_pressed() -> void:
 	print("deck button pressed")
-	
+
 
 func _on_play_button_pressed() -> void:
 	print("play button pressed")
@@ -25,15 +26,18 @@ func _on_quit_button_pressed() -> void:
 	print_rich("quit button pressed")
 	get_tree().quit()
 
+
 func _on_host_button_pressed() -> void:
 	if JoinMenu.visible:
 		JoinMenu.visible = false
 	HostMenu.visible = true
 
+
 func _on_join_button_pressed() -> void:
 	if HostMenu.visible:
 		HostMenu.visible = false
 	JoinMenu.visible = true
+
 
 func _on_host_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Board/board.tscn")
