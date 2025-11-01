0cab15bb09fb921da6fdf011a82181f9cb2e1878
unknown <yossi31018@gmail.com>
2025-11-01 19:55:52 +0200

    added hover sound

--- monster-cards-clone/Scripts/Board/board.gd
+++ monster-cards-clone/Scripts/Board/board.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var hand := $Hand
 @onready var card_slot_container := $CardSlotContainer
-@export var card_board_scene: PackedScene
+@export var board_card_scene: PackedScene
 @onready var sfx_place: AudioStreamPlayer = $sfx_place
 @onready var sfx_select: AudioStreamPlayer = $sfx_select
 @onready var sfx_deselect: AudioStreamPlayer = $sfx_deselect
@@ -29,7 +29,6 @@ func _place_card_into_slot(card: HandCard, slot: CardSlot):
 	slot.take()
 	card.goto_slot(slot)
 	card.tween.tween_callback(_replace_handcard_with_boardcard.bind(card))
-	sfx_place.play()
 	print("card placed")
 
 
@@ -38,10 +37,11 @@ func _replace_handcard_with_boardcard(card: HandCard):
 	Replaces the HandCard with a BoardCard object
 	This should be done after the card is moved into a slot
 	"""
-	var new_card_board := card_board_scene.instantiate()
-	add_child(new_card_board) #temporary, should add underneath some container and not directly
-	new_card_board.global_position = card.card_front.global_position
+	var new_board_card := board_card_scene.instantiate()
+	add_child(new_board_card) #temporary, should add underneath some container and not directly
+	new_board_card.global_position = card.card_front.global_position
 	card.queue_free()
+	sfx_place.play()
 
 
 func select_card(card: HandCard):
