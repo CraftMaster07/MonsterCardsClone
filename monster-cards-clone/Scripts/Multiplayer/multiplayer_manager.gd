7c605b09459360bb975fea7da23916f898e22b97
CraftMaster07 <mogobking@gmail.com>
2025-11-07 15:47:33 +0200

    added multiplayer 2
    not creating player scenes yet, or have any functionality beside connecting to each other🥀🥀🥀

--- monster-cards-clone/Scripts/Multiplayer/multiplayer_manager.gd
+++ monster-cards-clone/Scripts/Multiplayer/multiplayer_manager.gd
@@ -8,28 +8,20 @@ based on the player's role (host or joiner).
 
 signal start_game()
 
-# Scene to instantiate for the local player's game area (e.g., your board).
-@export var your_field_scene: PackedScene
-# Scene to instantiate for the remote player's game area (e.g., the enemy board).
-@export var enemy_field_scene: PackedScene
+
 @export var client_scene: PackedScene
 @export var server_scene: PackedScene
 
 var peer = ENetMultiplayerPeer.new()
-# The hardcoded IP address of the server/host.
+
 const SERVER_IP = "147.235.201.54"
-# The network port used for communication.
 const PORT = 59009
 
-@onready var host_button = get_parent().get_node("HostButton")
-@onready var join_button = get_parent().get_node("JoinButton")
 
-
-func _on_host_button_pressed() -> void:
+func host_game() -> void:
 	"""
 	Hosts a game as a server.
 	"""
-	disable_buttons()
 	var server = server_scene.instantiate()
 	add_child(server)
 
@@ -41,23 +33,17 @@ func _on_host_button_pressed() -> void:
 	multiplayer.peer_connected.connect(_on_peer_connected)
 	multiplayer.peer_disconnected.connect(_on_player_disconnected)
 
-	var your_player_scene = your_field_scene.instantiate()
-	add_child(your_player_scene)
 
 
-func _on_join_button_pressed() -> void:
+func join_game() -> void:
 	"""
 	Joins a game as a client.
 	"""
-	disable_buttons()
 	var client = client_scene.instantiate()
 	add_child(client)
 
 	peer.create_client(SERVER_IP, PORT)
 	multiplayer.multiplayer_peer = peer
-
-	var enemy_scene = enemy_field_scene.instantiate()
-	add_child(enemy_scene)
 
 
 func _on_peer_connected(id):
@@ -66,9 +52,8 @@ func _on_peer_connected(id):
 	@param id: The unique network ID of the connected peer.
 	"""
 	print("peer connected: ", id)
-	# The host instantiates the enemy field for the newly connected client
-	var enemy_scene = enemy_field_scene.instantiate()
-	add_child(enemy_scene)
+	# var enemy_scene = enemy_field_scene.instantiate()
+	# add_child(enemy_scene)
 
 
 func _on_player_disconnected(id):
@@ -78,15 +63,5 @@ func _on_player_disconnected(id):
 	print("peer disconnected: ", id)
 
 
-func disable_buttons():
-	"""
-	Disables the host and join buttons.
-	"""
-	host_button.disabled = true
-	host_button.visible = false
-	join_button.disabled = true
-	join_button.visible = false
-
-
-func start_game():
+func signal_start_game():
 	start_game.emit()
--- monster-cards-clone/gaem.gd
+++ monster-cards-clone/gaem.gd
@@ -4,7 +4,42 @@ extends Node
 @onready var main_menu = $MainMenu
 @export var board_scene: PackedScene
 
+var board: Board = null
 
-func init_board():
-    var new_board: Board = board_scene.instantiate()
-    add_child(new_board)
+
+func transition_main_menu_to_board():
+	stop_main_menu()
+	start_board()
+
+
+func transition_board_to_main_menu():
+	stop_board()
+	start_main_menu()
+
+
+func start_board():
+	board = board_scene.instantiate()
+	add_child(board)
+
+
+func stop_board():
+	remove_child(board)
+	board.queue_free()
+
+
+func start_main_menu():
+	add_child(main_menu)
+
+
+func stop_main_menu():
+	remove_child(main_menu)
+
+
+func _on_main_menu_join_game() -> void:
+	multiplayer_manager.join_game()
+	transition_main_menu_to_board()
+
+
+func _on_main_menu_host_game() -> void:
+	multiplayer_manager.host_game()
+	transition_main_menu_to_board()
