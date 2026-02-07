30e7b721429788ea5456cd77b57e707a8fa320a1
CraftMaster07 <mogobking@gmail.com>
2026-02-07 22:34:21 +0200

    moved stuff

---
+++ monster-cards-clone/Scripts/Board/round_manager.gd
@@ -0,0 +1,51 @@
+extends Node
+
+var current_player_index: int = 0
+var current_player_id: int
+var turn_order: Array[int]
+
+signal started_turn(player_id: int)
+signal send_started_turn()
+
+
+func start_turn(player_id: int):
+	current_player_id = player_id
+	print("turn: ", current_player_id)
+	started_turn.emit(current_player_id)
+
+
+func next_turn():
+	print("turn order: ", turn_order)
+	start_turn(turn_order[current_player_index])
+	current_player_index = (current_player_index + 1) % len(turn_order)
+
+
+func client_ended_turn(player_id: int):
+	if player_id != current_player_id:
+		return
+
+	next_turn()
+	send_started_turn.emit()
+
+
+func init_turn_order(player_ids: Array[int]):
+	turn_order = player_ids
+
+
+func is_player_turn(player_id: int):
+	return current_player_id == player_id
+
+
+func serialize():
+	return {
+		"current_player_index": current_player_index,
+		"current_player_id": current_player_id,
+		"turn_order": turn_order
+	}
+
+
+func deserialize(data: Dictionary):
+	current_player_index = data["current_player_index"]
+	current_player_id = data["current_player_id"]
+	turn_order = data["turn_order"]
+	start_turn(current_player_id)
