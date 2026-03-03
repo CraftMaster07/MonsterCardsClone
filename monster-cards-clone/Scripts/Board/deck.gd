4b51e7349cb4d2145731eff79005f026f1133894
CraftMaster07 <mogobking@gmail.com>
2026-03-03 16:57:21 +0200

    deck has card count

--- monster-cards-clone/Scripts/Board/deck.gd
+++ monster-cards-clone/Scripts/Board/deck.gd
@@ -1,47 +1,63 @@
 extends Control
 
 @export var deck_card_scene: PackedScene
+@export var cards_container: Control
+@export var card_count_label: RichTextLabel
+
+
+func _ready():
+	update_cards_count_label()
 
 
 func draw_card():
-    if get_child_count() > 0:
-        remove_card()
-        return true
-    return false
+	if get_child_count() > 0:
+		remove_card()
+		return true
+	return false
 
 
 func add_cards(cards_count: int):
-    for i in range(cards_count):
-        add_card()
+	for i in range(cards_count):
+		add_card()
 
 
 func add_card():
-    var new_card = deck_card_scene.instantiate()
-    add_child(new_card)
+	var new_card = deck_card_scene.instantiate()
+	cards_container.add_child(new_card)
+	update_cards_count_label()
 
 
 func remove_cards(cards_count: int):
-    for i in range(cards_count):
-        remove_card()
+	for i in range(cards_count):
+		remove_card()
 
 
 func remove_card():
-    if get_child_count() == 0:
-        push_error("There are no cards in the deck")
+	if get_card_count() == 0:
+		push_error("There are no cards in the deck")
 
-    var drawn_card = get_child(0)
-    remove_child(drawn_card)
-    drawn_card.queue_free()
+	var drawn_card = cards_container.get_child(0)
+	cards_container.remove_child(drawn_card)
+	drawn_card.queue_free()
+	update_cards_count_label()
 
 
 func serialize():
-    return {"cards_count": get_child_count()}
+	return {"cards_count": get_card_count()}
 
 
 func deserialize(data: Dictionary):
-    var card_diff = data["cards_count"] - get_child_count()
+	var card_diff = data["cards_count"] - get_card_count()
 
-    if card_diff > 0:
-        add_cards(card_diff)
-    elif card_diff < 0:
-        remove_cards(-card_diff)
+	if card_diff > 0:
+		add_cards(card_diff)
+	elif card_diff < 0:
+		remove_cards(-card_diff)
+
+
+func get_card_count():
+	return cards_container.get_child_count()
+
+
+func update_cards_count_label():
+	card_count_label.text = ColorConstants.RED_TEXT.format([str(get_card_count())])
