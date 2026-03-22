4e18c15ca4ff44937031b90b134e74a3f5358b4b
Rainmas <shmeritamar@gmail.com>
2026-03-22 23:18:37 +0200

    Can now save custom cards to file (wow)

--- monster-cards-clone/Scripts/Deck Customization/card_creator.gd
+++ monster-cards-clone/Scripts/Deck Customization/card_creator.gd
@@ -2,6 +2,28 @@ extends Control
 
 signal leave()
 
+var card_file = CardFile.new()
+@export var name_line_edit: LineEdit
+
 
 func _on_back_button_pressed() -> void:
 	leave.emit()
+
+
+func _on_health_spin_box_value_changed(value: float) -> void:
+	card_file.health = value
+
+
+func _on_attack_spin_box_value_changed(value: float) -> void:
+	card_file.attack = value
+
+func _on_cost_spin_box_value_changed(value: float) -> void:
+	card_file.cost = value
+
+
+func _on_save_button_pressed() -> void:
+	card_file.save()
+
+
+func _on_name_line_edit_text_changed(_new_text: String) -> void:
+	card_file.card_name = name_line_edit.text
--- monster-cards-clone/Scripts/Deck Customization/card_file.gd
+++ monster-cards-clone/Scripts/Deck Customization/card_file.gd
@@ -1,9 +1,26 @@
 extends RefCounted
 class_name CardFile
 
-const SAVE_PATH : String = "user://Cards/"
+const SAVE_PATH: String = "user://Cards/"
 
-var card_name: String
-var health: int
-var attack: int
-var cost: int
+var card_name: String = ""
+var health: int = 0
+var attack: int = 0
+var cost: int = 0
+
+
+func save():
+    if not DirAccess.dir_exists_absolute(SAVE_PATH):
+        DirAccess.make_dir_absolute(SAVE_PATH)
+    
+    var file_name := card_name.replace(" ", "_")
+    var file := FileAccess.open(SAVE_PATH + file_name + ".json", FileAccess.WRITE)
+    var data := {
+        "card_name": card_name,
+        "health": health,
+        "attack": attack,
+        "cost": cost
+    }
+    var stringified_data := JSON.stringify(data)
+    file.store_string(stringified_data)
+    file.close()
